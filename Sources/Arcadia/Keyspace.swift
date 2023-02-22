598430e1076dc90622b94c4db0576108076e52cd
Dr. Brandon Wiley <brandon@operatorfoundation.org>
2023-02-22 17:38:30 -0600

    Implemented getPeers() and fixed some locking

--- Sources/Arcadia/Keyspace.swift
+++ Sources/Arcadia/Keyspace.swift
@@ -11,14 +11,18 @@ import Abacus
 
 public class Keyspace
 {
+    // Private constants
     let keys: SortedSet<Key> = SortedSet<Key>(sortingStyle: .lowFirst)
 
+    // Private variables
     var lock = DispatchSemaphore(value: 1)
 
+    // Public initializers
     public init()
     {
     }
 
+    // Public functions
     public func add(key: Key)
     {
         defer
@@ -30,13 +34,47 @@ public class Keyspace
         self.keys.add(element: key)
     }
 
-    public func getPeers(for server: Key) -> [Key]
+    public func remove(key: Key)
     {
-        return self.keys.array
+        defer
+        {
+            self.lock.signal()
+        }
+        self.lock.wait()
+
+        self.keys.remove(element: key)
+    }
+
+    public func getPeers(for server: Key) throws -> [Key]
+    {
+        defer
+        {
+            self.lock.signal()
+        }
+        self.lock.wait()
+
+        var set: Set<Key> = Set<Key>()
+
+        let next = try getNext(for: server)
+        let previous = try getPrevious(for: server)
+        set.insert(next)
+        set.insert(previous)
+        set.insert(try getNext(for: next))
+        set.insert(try getPrevious(for: previous))
+
+        var results: [Key] = []
+        let _ = set.map { results.append($0) }
+        return results
     }
 
     public func getServers(for client: Key) throws -> [Key]
     {
+        defer
+        {
+            self.lock.signal()
+        }
+        self.lock.wait()
+
         var set: Set<Key> = Set<Key>()
 
         set.insert(try getNext(for: client))
@@ -49,6 +87,7 @@ public class Keyspace
         return results
     }
 
+    // Private functions
     func getShiftedLeft(for key: Key) throws -> [Key]
     {
         let (idealAKey, idealBKey) = key.shiftLeft()
@@ -100,6 +139,7 @@ public class Keyspace
     }
 }
 
+// Errors
 public enum KeyspaceError: Error
 {
     case keyLookupFailed
