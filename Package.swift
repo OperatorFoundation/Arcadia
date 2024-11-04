344f841bfb4290f662d5525b91e095a6f2574253
bragelbytes <joebragel@gmail.com>
2024-11-03 17:11:03 -0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -15,13 +15,13 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/OperatorFoundation/Abacus", from: "1.0.3"),
+        .package(url: "https://github.com/OperatorFoundation/Abacus", from: "1.0.4"),
         .package(url: "https://github.com/OperatorFoundation/Bits", from: "2.0.4"),
         .package(url: "https://github.com/OperatorFoundation/Datable", from: "4.0.1"),
-        .package(url: "https://github.com/OperatorFoundation/Keychain", from: "1.0.2"),
-        .package(url: "https://github.com/OperatorFoundation/Nametag", from: "0.1.2"),
-        .package(url: "https://github.com/OperatorFoundation/Swift-BigInt", from: "1.0.1"),
-        .package(url: "https://github.com/OperatorFoundation/Transmission", from: "1.2.11"),
+        .package(url: "https://github.com/OperatorFoundation/Keychain", from: "1.0.4"),
+        .package(url: "https://github.com/OperatorFoundation/Nametag", from: "0.1.4"),
+        .package(url: "https://github.com/OperatorFoundation/Swift-BigInt", from: "1.0.2"),
+        .package(url: "https://github.com/OperatorFoundation/Transmission", from: "1.2.13"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
